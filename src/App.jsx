8d5fef51417f43c4452683f7a3933995da58201f
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-02 14:52:32 +0500

    added 3rd problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 function App() {
+
+  // ---------------- Problem 1
+
   // let [path, setPath] = useState({
   //   routerBaseName: "/",
   //   source: {
@@ -25,37 +28,69 @@ function App() {
   // }, []);
   
   // console.log(path);
-  let [path, setPath] = useState([
+
+  // ---------------- Problem 2
+
+  // let [path, setPath] = useState([
+  //   {
+  //     routerBaseName: "/",
+  //     source: {
+  //       name: "name",
+  //       configuration: {
+  //         id: "123",
+  //         path: "/",
+  //       },
+  //     },
+  //   },
+  // ]);
+
+  // useEffect(() => {
+  //   setPath((others) => [
+  //     {
+  //       ...others[0],
+  //       source: {
+  //         ...others[0].source,
+  //         configuration: {
+  //           ...others[0].source.configuration,
+  //           path: "/xylexa",
+  //         },
+  //       },
+  //     },
+  //   ]);
+  // }, []);
+
+  // console.log(path);
+  // ---------------- Problem 3
+
+  let [path, setPath] = useState(
     {
       routerBaseName: "/",
-      source: {
+      source: [{
         name: "name",
         configuration: {
           id: "123",
           path: "/",
         },
-      },
+      }],
     },
-  ]);
+  );
 
   useEffect(() => {
-    setPath((others) => [
-      {
-        ...others[0],
-        source: {
-          ...others[0].source,
-          configuration: {
-            ...others[0].source.configuration,
-            path: "/xylexa",
-          },
-        },
-      },
-    ]);
+    setPath((others) => ({
+      ...others,
+      source: [{
+        ...others.source[0],
+        configuration: {
+          ...others.source[0].configuration,
+          path: "/xylexa",
+        }
+      }]
+    }));
   }, []);
 
   console.log(path);
 
-  // console.log(obj)
+  
 
   return (
     <>
